52ccce594a678925972be35ad3b951e82b06ffe9
tianyy <407496032@qq.com>
2017-08-11 21:42:23 +0800

    modify look service

--- trunk/javaServer/src/main/java/com/tian/server/model/MudObject.java
+++ trunk/javaServer/src/main/java/com/tian/server/model/MudObject.java
@@ -1,7 +1,26 @@
 package com.tian.server.model;
 
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by PPX on 2017/8/8.
  */
 public class MudObject {
+
+    public Map<String, Object> attributes = new HashMap<String, Object>();
+    public Map<String, Object> temp = new HashMap<String, Object>();
+    protected Map<String, Object> apply = new HashMap<String, Object>();
+
+    public Object queryTemp(String key){
+
+        return temp.get(key);
+    }
+
+    public Object query(String key){
+
+        return attributes.get(key);
+    }
 }
