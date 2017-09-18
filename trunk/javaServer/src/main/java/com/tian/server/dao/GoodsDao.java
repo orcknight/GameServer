0382ddfa33e45c1afbcb2da781146bb5331b24e2
tianyy <407496032@qq.com>
2017-09-18 21:27:35 +0800

    xx

---
+++ trunk/javaServer/src/main/java/com/tian/server/dao/GoodsDao.java
@@ -0,0 +1,21 @@
+package com.tian.server.dao;
+
+import com.tian.server.entity.GoodsEntity;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+/**
+ * Created by PPX on 2017/9/18.
+ */
+public class GoodsDao extends BaseDao {
+
+    public List<GoodsEntity> getList(){
+
+        String queryStr = "SELECT * FROM city";
+        Query q = getSession().createNativeQuery(queryStr).addEntity(GoodsEntity.class);
+        List<GoodsEntity> list = q.getResultList();
+
+        return list;
+    }
+}
